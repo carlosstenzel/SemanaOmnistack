40fb5cb7f224594731fec5d48a51e9f1ab5f68fb
carlosstenzel <carlosstenzel@hotmail.com>
2019-08-06 23:22:01 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
-
+const server = express();
 
 mongoose.connect(
     process.env.URL_BD_MONGO,
@@ -12,8 +13,8 @@ mongoose.connect(
     }
 );
 
-const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
